5175f2b5861c84ac036678c59f40803a4d81f4fa
Alexander Gryaznov <hi@agryaznov.com>
2023-12-09 12:32:43 +0200

    fix

--- substrate/frame/indices/src/lib.rs
+++ substrate/frame/indices/src/lib.rs
@@ -284,12 +284,12 @@ pub mod pallet {
 		}
 	}
 
-	#[cfg(feature = "try-runtime")]
 	#[pallet::hooks]
 	impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {
+		#[cfg(feature = "try-runtime")]
 		fn try_state(_: BlockNumber) -> Result<(), TryRuntimeError> {
 			log::info!(target: LOG_TARGET, "--------------- INDICES: --------------");
-			for (k,v) in <Accounts<T>>::iter() {
+			for (k, v) in <Accounts<T>>::iter() {
 				log::info!(target: LOG_TARGET, "Index {k} belongs to {:?}", k, v.0.to_string());
 			}
 		}
